3610db82a23be7ef83fb7abcb4f815d59c83193a
chohee <joy@olulo.io>
2022-10-26 23:11:16 +0900

    #19 버그 픽스
    - intent 수정
    - 월렛 연결 시 버그 수정

--- nextjs/moveshop/components/itemList.tsx
+++ nextjs/moveshop/components/itemList.tsx
@@ -5,16 +5,16 @@ import Item from "./item";
 import {ItemListProps} from "../interface/props/itemListProps";
 
 const ItemList: NextPage<ItemListProps> = (props) => {
-    return (
-        <Stack  direction={{ xs: 'column', sm: 'row' }}
-                spacing={{ xs: 1, sm: 2, md: 4 }}>
-            <Item key={1} value={1} title={"안녕"}/>
-            <Item key={1} value={1} title={"안녕"}/>
-            <Item key={1} value={1} title={"안녕"}/>
-            <Item key={1} value={1} title={"안녕"}/>
-            <Item key={1} value={1} title={"안녕"}/>
-        </Stack>
-    )
+  return (
+    <Stack direction={{xs: 'column', sm: 'row'}}
+           spacing={{xs: 1, sm: 2, md: 4}}>
+      <Item key={1} value={1} title={"안녕"}/>
+      <Item key={1} value={1} title={"안녕"}/>
+      <Item key={1} value={1} title={"안녕"}/>
+      <Item key={1} value={1} title={"안녕"}/>
+      <Item key={1} value={1} title={"안녕"}/>
+    </Stack>
+  )
 }
 
 export default ItemList
