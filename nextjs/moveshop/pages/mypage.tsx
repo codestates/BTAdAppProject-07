9489f76e72a48d3a043bf326d1df87be2e18caeb
chohee <joy@olulo.io>
2022-10-27 19:34:21 +0900

    #18 네비게이션 링크 추가

--- nextjs/moveshop/pages/mypage.tsx
+++ nextjs/moveshop/pages/mypage.tsx
@@ -1,9 +1,62 @@
+import {Box, Tab, Tabs, Typography} from "@mui/material";
+import {ReactNode, SyntheticEvent, useState} from "react";
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel = (props: { index: number, value: any, children: ReactNode }) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`collection-tabpanel-${index}`}
+      aria-labelledby={`collection-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+const a11yProps = (index: number) => {
+  return {
+    id: `collection-${index}`,
+    'aria-controls': `collection-tabpanel-${index}`,
+  };
+}
 
 export default function MyPage() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <>
-      마이 페이지
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="user tabs">
+            <Tab label="Collected" {...a11yProps(0)} />
+            <Tab label="Created" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+      </Box>
     </>
   )
 }
